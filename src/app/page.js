b4e2796c8cb2c517f1ebbf4e95cc37a13519dbd0
Alejandro Lucena <a.lucena86@gmai.com>
2025-08-08 20:55:41 +0200

    colors

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center bg-blue-500 text-white font-bold">
       <div className="max-w-6xl py-20 px-5 min-h-screen flex flex-col justify-center items-center">
-        <h1>Entre Nubes</h1>
+        <h1 className="text-3xl">Entre Nubes</h1>
         <p>Asesoramiento de Sueño Infantil Respetuso</p>
       </div>
     </div>
